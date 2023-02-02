3d5847eb03050582816fb15a7d10f9d8bc2f4c72
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-02-02 14:30:53 -0500

    basic design of journal app

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,7 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
@@ -9,9 +10,25 @@ export const JournalPage = () => {
        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi soluta culpa excepturi vero ducimus iusto dolores voluptatum error, quisquam labore harum perferendis provident in placeat beatae magnam tempore quaerat iure.
       </Typography> */}
 
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
 
-      
+      <NoteView />
+
+      <IconButton
+       size='large'
+       sx={{
+         color: 'white',
+         backgroundColor: 'error.main',
+         ':hover': { backgroundColor: 'error.main', opacity: 0.9},
+         position: 'fixed',
+         right: 50,
+         bottom: 50
+       }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
+
     </JournalLayout>
   )
 }
