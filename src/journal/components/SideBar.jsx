eb360e26c0309b0a8f0d9c3f94d25d3985ef2337
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-02-15 10:41:31 -0500

    upload note and active note

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,12 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux";
+import { SideBarItem } from "./SideBarItem";
 
 export const SideBar = ({ drawerWidth }) => {
 
   const { displayName } = useSelector( state => state.auth );
+  const { notes } = useSelector( state => state.journal );
 
   return (
     <Box
@@ -28,18 +30,8 @@ export const SideBar = ({ drawerWidth }) => {
 
         <List>
           {
-            ['January', 'February', 'March', 'April'].map( text => (
-              <ListItem key={ text } disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TurnedInNot />
-                  </ListItemIcon>
-                  <Grid container>
-                    <ListItemText primary={ text }/>
-                    <ListItemText secondary={ 'lorem impuson minunsin' } />
-                  </Grid>
-                </ListItemButton>
-              </ListItem>
+            notes.map( note => (
+              <SideBarItem key={ note.id } { ...note }/>
             ))
           }
         </List>
